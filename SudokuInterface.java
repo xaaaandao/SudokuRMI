2d24aff1bff7855dd5a86b58b9bbe09958011f3d
Alexandre Kajihara <alexandre.ykz@gmail.com>
2017-10-31 02:02:35 -0200

    Trocando o nome da matrix #1

--- SudokuInterface.java
+++ SudokuInterface.java
@@ -2,6 +2,8 @@ import java.rmi.*;
 import java.util.*;
 
 public interface SudokuInterface extends Remote {
+	public int countHit() throws RemoteException;
+	public int countError() throws RemoteException;
     public int [][]matrixForUser() throws RemoteException;
-    public void checkInput(int i, int j) throws RemoteException;
+    public int checkInput(int value, int i, int j) throws RemoteException;
 }
